14c4d51dcdd0561744fcb336a184cc643749af91
SkInjamamulIslam <injamamulislam017@gmail.com>
2024-02-26 21:17:04 +0100

    If-else

--- injamam.py
+++ injamam.py
@@ -1,4 +1,9 @@
-I ma f falmc.amc afncfakc
-djkajcn jadnm,anca,c
-cfxbgfhgvjhm
-/newfile
+if (5>4):
+    print ("five is less than four");
+else:
+    print ("five is greater than four");
+x=4
+y=5
+print (x+y)
+    
+    
