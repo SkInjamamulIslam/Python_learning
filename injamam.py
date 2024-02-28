d2c1b525826fbd566283e05bbb0bb2d02590e4a5
SkInjamamulIslam <injamamulislam017@gmail.com>
2024-02-28 20:59:52 +0100

    Global Variables

--- injamam.py
+++ injamam.py
@@ -1,9 +1,19 @@
-if (5>4):
-    print ("five is less than four");
-else:
-    print ("five is greater than four");
-x=4
-y=5
-print (x+y)
-    
-    
+name = "Bioinformatics"
+number = 21
+source = 3.5
+analyse_List = ["GenaA", "GenaB","GeneC", "GeneD"]
+new_gene = analyse_List[3]
+print(new_gene)
+print(type (source))
+name,number,source = "Bioinformatics", 21, 3.5
+print(name)
+print(source)
+print(analyse_List[1]+","+analyse_List[3])
+x = "awesome"
+def myfunc():
+ print ("Python "+ "5")
+myfunc()
+x = "awesome"
+def myfunc():
+ return "Python "+ "5"
+print (myfunc())
